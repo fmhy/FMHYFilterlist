d2b6cfa268765a443ff5febc8b9269703cbb4d6c
WindowsAurora <104532354+WindowsAurora@users.noreply.github.com>
2024-09-30 19:21:04 +0100

    Fix typo

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   outputs = { self, nixpkgs }: let
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
   in {
-    devShells."x86-64-linux".default = pkgs.mkShell {
+    devShells."x86_64-linux".default = pkgs.mkShell {
       packages = [ pkgs.python3 ];
     };
   };
